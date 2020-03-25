a3930a6d999ec5b9e58fc5d1c5637ca2b53da806
李博 <libo@51gwd.com>
2020-03-25 17:24:48 +0800

    dnspod做ddns服务

---
+++ dnspod_api/ddns.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#添加域名信息
+function add_domin()
+{
+curl -X POST https://dnsapi.cn/Domain.Create -d 'login_token=cfacc360bc7588e959394e481fe54b24
+&domain=fuckme.ingogogo.com&format=json'
+}
+
+#获取域名信息 
+
+function get_domin_info()
+{
+curl -X POST https://dnsapi.cn/Domain.Info -d 'login_token=148346，cfacc360bc7588e959394e481fe54b24&format=json&domain_id=71663193'
+}
+
+#ingogo的domain_id为
+71663193
+
+#获取记录列表
+function get_domain()
+{
+curl -X POST https://dnsapi.cn/Record.List -d 'login_token=148346,cfacc360bc7588e959394e481fe54b24&format=json&domain_id=71663193&sub_domain=fuckme&record_type=A&offset=0&length=3'|jq
+}
+
+#record_id 561684631
+#修改记录
+
+function modify_record()
+{
+ip=$1
+curl -X POST https://dnsapi.cn/Record.Modify -d "login_token=148346,cfacc360bc7588e959394e481fe54b24&format=json&domain_id=71663193&record_id=561684631&sub_domain=fuckme&value=${ip}&record_type=A&record_line_id=10%3D0"
+}
+
+
+
+#获取外网ip
+
+content=`wget -q -O - http://icanhazip.com/|sed 's/\.//g'`
+content_ip_record=`ping fuckme.ingogogo.com -c 1|grep -oP '(?<=\().*(?=\):)'|sed 's/\.//g'`
+
+function judge()
+{
+if [ $content = $content_ip_record ]; 
+	then echo 11; 
+else echo 22; 
+fi
+}	
