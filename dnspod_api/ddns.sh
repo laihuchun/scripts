10be1f7dde0d3d939d4bf1845efa3433e283d2b0
李博 <libo@51gwd.com>
2020-04-09 20:11:34 +0800

    修改record值record_id改为默认，如用原来线路为电信，出现未知的名称或服务的错误

--- dnspod_api/ddns.sh
+++ dnspod_api/ddns.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+id="你的id"
+api_token="你的token"
+#domain_id和record_id没写函数，自己请求一下
+
 real_ip=`wget -q -O - http://icanhazip.com/`
-content=`echo $real_ip|sed 's/\.//g'`
-content_ip_record=`ping fuckme.ingogogo.com -c 1|grep -oP '(?<=\().*(?=\) \d)'|sed 's/\.//g'`
-dns_ip_len=`echo $content_ip_record|wc -l`
-if [ ${#dns_ip_len} -eq 5 ];
-then content_ip_record='114114114114'
-fi
+real_ip_bijiao=`echo $real_ip|sed 's/\.//g'`
+dns_ip=
+dns_ip_bijiao=
+
 # api_token 去这里申请 https://www.dnspod.cn/console/user/security 
 
-
-api_token="API_TOKEN"
-id="ID"
 
 #添加域名信息
 function add_domin()
@@ -19,17 +18,24 @@ curl -X POST https://dnsapi.cn/Domain.Create -d "login_token=$api_token
 &domain=fuckme.ingogogo.com&format=json"
 }
 
+#获取你的域名ip，以便判断避免相同记录重复提交
+function get_ip4_dns()
+   {
+    dns_ip=`curl -s -X POST https://dnsapi.cn/Record.List -d "login_token=$id,$api_token&format=json&domain_id=71663193&sub_domain=fuckme&record_type=A&offset=0&length=3"|grep -oP '(?<=value":").*(?=","enable)'`
+    dns_ip_bijiao=`echo $dns_ip|sed 's/\.//g'`
+    }
+
 #获取域名信息 
 
 function get_domin_info()
 {
-curl -X POST https://dnsapi.cn/Domain.Info -d "login_token=$id,$api_token&format=json&domain_id=71663193"
+curl -s -X POST https://dnsapi.cn/Domain.Info -d "login_token=$id,$api_token&format=json&domain_id=71663193"
 }
 
 #获取记录列表
 function get_domain()
 {
-curl -X POST https://dnsapi.cn/Record.List -d "login_token=$id,$api_token&format=json&domain_id=71663193&sub_domain=fuckme&record_type=A&offset=0&length=3"|jq
+curl -s -X POST https://dnsapi.cn/Record.List -d "login_token=$id,$api_token&format=json&domain_id=71663193&sub_domain=fuckme&record_type=A&offset=0&length=3"|jq
 }
 
 #修改记录
@@ -37,7 +43,7 @@ curl -X POST https://dnsapi.cn/Record.List -d "login_token=$id,$api_token&format
 function modify_record()
 {
 ip=$1
-curl -X POST https://dnsapi.cn/Record.Modify -d "login_token=$id,$api_token&format=json&domain_id=71663193&record_id=561684631&sub_domain=fuckme&value=${ip}&record_type=A&record_line_id=10%3D0"
+curl -s -X POST https://dnsapi.cn/Record.Modify -d "login_token=$id,$api_token&format=json&domain_id=71663193&record_id=561684631&sub_domain=fuckme&value=${ip}&record_type=A&record_line=默认"
 }
 
 
@@ -48,7 +54,7 @@ curl -X POST https://dnsapi.cn/Record.Modify -d "login_token=$id,$api_token&form
 
 function judge()
 {
-if [ $content == $content_ip_record ];
+if [ $real_ip_bijiao == $dns_ip_bijiao ];
 	then echo 'do nothing';
 else modify_record $real_ip;
 fi
