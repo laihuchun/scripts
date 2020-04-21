7c8a1f0780d9ed64f056606197871af226831232
李博 <libo@51gwd.com>
2020-04-21 19:27:36 +0800

    从建行网站获取贵金属价格

---
+++ get_precious_metals_price.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#  品种code
+#					<option value="019995">AU9995</option>
+#					<option value="019999">AU9999</option>
+#					<option value="030001">人民币铂</option>
+#					<option value="020001">人民币银</option>
+#					<option value="030002">美元铂(钞)</option>
+#					<option value="030003">美元铂(汇)</option>
+#					<option value="010002">美元金(钞)</option>
+#					<option value="010003">美元金(汇)</option>
+#					<option value="020002">美元银(钞)</option>
+#					<option value="020003">美元银(汇)</option>
+#                    <option value="999927">人民币钯</option>
+#                    <option value="999928">美元钯(钞)</option>
+#                    <option value="999929">美元钯(汇)</option>
+
+curl -s 'http://tool.ccb.com/webtran/static/trendchart/getAccountData.gsp?dateType=timeSharing&sec_code=020001'|grep -oP '(?<="new_pri":")[0-9.]*'
